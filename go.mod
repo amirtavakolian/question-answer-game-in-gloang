63c33f238ad7f9128b8a387c64d1cb49e1bcb44a
Amir Tavakolian <rezatva@gmail.com>
2025-08-17 18:30:03 +0330

    add mysql package

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module QA-Game
 
 go 1.23.1
+
+require github.com/go-sql-driver/mysql v1.9.3
+
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ repository/mysql/mysql.go
@@ -0,0 +1,28 @@
+package mysql
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
+)
+
+type Mysql struct {
+	DB *sql.DB
+}
+
+func NewMysql() *Mysql {
+
+	// todo => remove user pass (prevent from hard-coding)
+	db, err := sql.Open("mysql", "root:@(localhost:3306)/question-game?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return &Mysql{DB: db}
+}
