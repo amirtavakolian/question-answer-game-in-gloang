d537e7111ea41c6aa8245e8a17cad8113c039e9c
Amir Tavakolian <rezatva@gmail.com>
2025-08-21 11:00:34 +0330

    add validate and get features to jwt service

--- services/jwttoken/jwt.go
+++ services/jwttoken/jwt.go
@@ -3,32 +3,115 @@ package jwttoken
 import (
 	"github.com/golang-jwt/jwt/v5"
 	"log"
+	"time"
 )
 
-type JwtToken struct {
-	SignedKey string
+const (
+	AccessTokenSubject  = "access-token"
+	RefreshTokenSubject = "refresh-token"
+)
+
+type MyCustomClaims struct {
+	PhoneNumber string `json:"phone_number"`
+	jwt.RegisteredClaims
 }
 
-func NewJwtToken() JwtToken {
-	return JwtToken{SignedKey: "a-random-key"}
-	// todo => move signed key to env
+type JwtResponse struct {
+	Status  bool
+	Message string
+	Data    interface{}
 }
 
-func (jwtToken JwtToken) CreateToken(phoneNumber string) string {
+type JwtService struct {
+	signedKey             []byte
+	accessExpirationTime  *jwt.NumericDate
+	refreshExpirationTime *jwt.NumericDate
+}
 
-	mySigningKey := []byte("golang-programming-language")
+func NewJwtToken() JwtService {
+
+	signinKey := []byte("@@##AAAtt##$@#@%23432424asdsad345345SFD")
+
+	return JwtService{
+		signedKey:             signinKey,
+		accessExpirationTime:  jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 7)),
+		refreshExpirationTime: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 10)),
+	}
+}
+
+func (jwtToken JwtService) CreateAccessToken(phoneNumber string) string {
+	return jwtToken.createToken(phoneNumber, AccessTokenSubject)
+}
+
+func (jwtToken JwtService) CreateRefreshToken(phoneNumber string) string {
+	return jwtToken.createToken(phoneNumber, RefreshTokenSubject)
+}
+
+func (jwtToken JwtService) createToken(phoneNumber string, subject string) string {
 
 	anotherClaim := jwt.MapClaims{
 		"phone_number": phoneNumber,
+		"exp":          jwtToken.accessExpirationTime,
+		"sub":          subject,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, anotherClaim)
 
-	signedToken, err := token.SignedString(mySigningKey)
+	signedToken, err := token.SignedString(jwtToken.signedKey)
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	return signedToken
+}
+
+func (jwtToken JwtService) Get(tokenString string) (bool, string, *MyCustomClaims) {
+
+	jwtResponse := jwtToken.ValidateTokenExpiration(tokenString)
+
+	if !jwtResponse.Status {
+		return false, jwtResponse.Message, nil
+	}
+
+	if claims, ok := jwtResponse.Data.(*jwt.Token).Claims.(*MyCustomClaims); ok {
+		return true, "", claims
+	} else {
+		return false, "unknown claims type, cannot proceed", nil
+	}
+}
+
+func (jwtToken JwtService) ValidateTokenExpiration(tokenString string) JwtResponse {
+
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(jwtToken.signedKey), nil
+	})
+
+	if err != nil {
+		return JwtResponse{
+			Status:  false,
+			Message: "token signature is invalid",
+		}
+	}
+
+	expirationTime, expirationTimeErr := token.Claims.GetExpirationTime()
+
+	if expirationTimeErr != nil {
+		return JwtResponse{
+			Status:  false,
+			Message: "Error in calculating expiration time",
+		}
+	}
+
+	if expirationTime.Time.Before(time.Now()) {
+		return JwtResponse{
+			Status:  false,
+			Message: "Token is expired",
+		}
+	}
+
+	return JwtResponse{
+		Status: true,
+		Data:   token,
+	}
 }
